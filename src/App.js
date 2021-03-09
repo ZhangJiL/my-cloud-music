042510f9cb9acf5de79eaf9d8e86ae8946e5bcf7
zhangjilei <zhangjilei@iqihang.com>
2021-03-09 21:15:51 +0800

    feat: 搭建项目基本骨架

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { GlobalStyle } from  './style';
+import { renderRoutes } from 'react-router-config';
+import routes from './routes/index.js';
 import { IconStyle } from './assets/iconfont/iconfont';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store/index'
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+      </HashRouter>
+    </Provider>
   );
 }
 
